628708532bb0b2f1c46bc3b340fdf578022afab7
i6401566 <n.greenberg@student.maastrichtuniversity.nl>
2026-04-09 17:10:50 +0200

    Changes to be committed: 	modified:   src/WorldState.java

--- src/WorldState.java
+++ src/WorldState.java
@@ -3,11 +3,14 @@ import java.util.LinkedList;
 
 class WorldState {
     private static final LinkedList<String>[] worldState = new List[];
-
+    //LinkedList type, num in array
+    //Everything is nonvoid
     public static void main (String[] args) {
         initializeWorld(100, 100, 100, 0);
     }
 
+    // Method must output the variable
+    
     private static void initializeWorld(int trees, int fish, int fullness, int campfire) {
         worldState.add("trees", trees);
         worldState.add("fish", fish);
