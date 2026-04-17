96d591caf534336ca2eef40c8f7466eda3b57ce5
aconCS <2005acon@gmail.com>
2026-04-17 23:29:55 +0200

    Changed main's method signature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,13 @@
-class Main {
-  String[][] world = iniitalizeWorld(100, 100, 0, 0);
-  int maxDailyCost = 10;
-  
-  // So-far cost from start to selected point
-  // Must be less than maxDailyCost
-  int actualCost = 0;
+public static void main(String... args) {
+    String[][] world = iniitalizeWorld(100, 100, 0, 0);
+    int maxDailyCost = 10;
 
-  boolean running = True;
-  while(running) {
-    running = checkIfGameOn(world);
+    // So-far cost from start to selected point
+    // Must be less than maxDailyCost
+    int actualCost = 0;
 
-    
-  }
+    boolean running = True;
+    while (running) {
+        running = checkIfGameOn(world);
+    }
 }
