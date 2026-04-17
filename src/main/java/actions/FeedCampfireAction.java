cbe0b536a378b958741eb033fee03091f1118ab1
Raistlin0905 <idean8569@gmail.com>
2026-04-17 19:38:38 +0200

    Completed all the action classes

--- src/main/java/actions/FeedCampfireAction.java
+++ src/main/java/actions/FeedCampfireAction.java
@@ -1,3 +1,20 @@
 package actions;
 
-public final record FeedCampfireAction (String action, int cost, String effect, String[] preReq) implements Action {}
+import states.*;
+
+public final record FeedCampfireAction (String actionName, int cost) implements Action {
+
+    public String getName() { return actionName; }
+    public int getCost() { return cost; }
+
+    public boolean checkIfAllowed(WorldState worldState, String agentName) {
+        AgentState agentState = worldState.getAgentState(agentName);
+        return agentState != null && agentState.getFish() > 0;
+    }
+
+    public WorldState executeAction(WorldState worldState, String agentName) {
+        AgentState firstAgentState = worldState.getAgentState(agentName);
+        AgentState secondAgentState = firstAgentState.changeCampfire(5);
+        return worldState.changeAgentState(agentName, secondAgentState);
+    }
+}
--- src/main/java/actions/FishAction.java
+++ src/main/java/actions/FishAction.java
@@ -1,3 +1,22 @@
 package actions;
 
-public final record FishAction (String action, int cost, String effect, String[] preReq) implements Action {}
+import states.*;
+
+public final record FishAction (String actionName, int cost) implements Action {
+
+    public String getName() { return actionName; }
+    public int getCost() { return cost; }
+
+    public boolean checkIfAllowed(WorldState worldState, String agentName) {
+        AgentState agentState = worldState.getAgentState(agentName);
+        return agentState != null && worldState.getTrees() > 0;
+    }
+
+    public WorldState executeAction(WorldState worldState, String agentName) {
+        AgentState firstAgentState = worldState.getAgentState(agentName);
+        AgentState secondAgentState = firstAgentState.changeFishCount(1);
+        WorldState firstWorldState = worldState.changeAgentState(agentName, secondAgentState);
+        WorldState secondWorldState = firstWorldState.changeFishCount(-1);
+        return secondWorldState;
+    }
+}
